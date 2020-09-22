6cb3e57f7792f36e47eb184008c774fc834745ae
Vinicius Komninakis <vinicius.komninakis@luizalabs.com>
2020-09-21 21:55:00 -0300

    feat: convert args

--- src/conversor/core.clj
+++ src/conversor/core.clj
@@ -1,7 +1,9 @@
 (ns conversor.core
+  (:require [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
+(def program-options [["-f" "--from coin from"] ["-t" "--to coin to"]])
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (:options (parse-opts args program-options)))
