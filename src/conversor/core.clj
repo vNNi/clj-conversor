516f0ec4ff632ab4d020f67f249f07e522373b4e
Vinicius Komninakis <vinicius.komninakis@luizalabs.com>
2020-09-22 21:18:50 -0300

    feat: add http-client get to currency api

--- src/conversor/core.clj
+++ src/conversor/core.clj
@@ -1,9 +1,21 @@
 (ns conversor.core
-  (:require [clojure.tools.cli :refer [parse-opts]])
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            [clj-http.client :as http-client])
   (:gen-class))
 
 (def program-options [["-f" "--from coin from"] ["-t" "--to coin to"]])
 
+(def apiKey "5a90e48669a83c0e96fa")
+
+(def apiUrl "https://free.currconv.com/api/v7/convert")
+
+(defn parse-argument-to-query 
+  [from to]
+  (str from "_" to))
+
+
 (defn -main
   [& args]
-  (:options (parse-opts args program-options)))
+  (let [ {:keys [from to]} (:options (parse-opts args program-options)) ]
+  (println (http-client/get apiUrl {:query-params { "q" (parse-argument-to-query from to) 
+                                            "apiKey" apiKey }})))
